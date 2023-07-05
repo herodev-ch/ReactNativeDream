ef0df6a24943ca9c39a1529e7b6d875ac912747a
sawy22417 <sawy22417@gmail.com>
2023-07-05 09:45:21 +0300

    lesson5/delete-request

--- App.tsx
+++ App.tsx
@@ -1,21 +1,34 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-import axios from "axios"
+import {Button, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import axios from 'axios';
 
 const App = () => {
-
+  const URL = 'https://649ea51c245f077f3e9cb5bc.mockapi.io/books';
 
   const getBooks = async () => {
-    const response = await axios.get("https://649ea51c245f077f3e9cb5bc.mockapi.io/books")
+    const response = await axios.get(URL);
     console.log(JSON.stringify(response.data, null, 3));
-    
-  }
+  };
 
-  getBooks()
+  useEffect(() => {
+    getBooks();
+  }, []);
+
+  const addBook = async () => {
+    const response = await axios.post(URL, {
+      author: 'john',
+      cover: 'https://img.wattpad.com/cover/57258457-288-k474609.jpg',
+      name_of_book: 'sky book',
+    });
+    console.log(JSON.stringify(response.data, null, 3));
+  };
 
   return (
     <View style={styles.cont}>
-      <Text style={styles.welcomeText}>Welcome To {"\n"} HeroDev Youtube Channel</Text>
+      <Text style={styles.welcomeText}>
+        Welcome To {'\n'} HeroDev Youtube Channel
+      </Text>
+      <Button title="add book" onPress={addBook} />
     </View>
   );
 };
@@ -30,6 +43,6 @@ const styles = StyleSheet.create({
   },
   welcomeText: {
     fontSize: 23,
-    textAlign:"center"
+    textAlign: 'center',
   },
 });
