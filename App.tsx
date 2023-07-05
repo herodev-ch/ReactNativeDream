6a6bd1994b8404fc8a81ef390f57b8834f66cbde
sawy22417 <sawy22417@gmail.com>
2023-07-05 08:49:38 +0300

    lesson 4 get request

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import axios from "axios"
 
 const App = () => {
+
+
+  const getBooks = async () => {
+    const response = await axios.get("https://649ea51c245f077f3e9cb5bc.mockapi.io/books")
+    console.log(JSON.stringify(response.data, null, 3));
+    
+  }
+
+  getBooks()
+
   return (
     <View style={styles.cont}>
-      <Text style={styles.welcomeText}>Welcome To HeroDev Youtube Channel</Text>
+      <Text style={styles.welcomeText}>Welcome To {"\n"} HeroDev Youtube Channel</Text>
     </View>
   );
 };
@@ -19,5 +30,6 @@ const styles = StyleSheet.create({
   },
   welcomeText: {
     fontSize: 23,
+    textAlign:"center"
   },
 });
