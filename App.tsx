a54609fd601cfe76dec40e56bf466345105eac43
sawy22417 <sawy22417@gmail.com>
2023-08-23 22:29:52 +0300

    finish custom button

--- App.tsx
+++ App.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import Home from './src/screens/Home';
+import MainButton from './src/components/MainButton';
+import {View} from 'react-native';
 
 const App = () => {
-  return <Home />;
+  const onPressFN = () => {
+    console.log('Pressed FN');
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+      }}>
+      <MainButton title="Save" onPress={onPressFN} disable={true} />
+    </View>
+  );
 };
 
 export default App;
