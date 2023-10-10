2ba0659d9f5c0654facc7f745b2a38eaba791dd6
sawy22417 <sawy22417@gmail.com>
2023-10-10 19:08:35 +0300

    finish notifications

--- App.tsx
+++ App.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
-import SignUpScreen from './src/screens/SignUpScreen';
-import { NavigationContainer } from '@react-navigation/native';
+import React, { useEffect } from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 import MyStack from './src/navigation/MyStack';
+import messaging from '@react-native-firebase/messaging';
 
 const App = () => {
+
+  async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
+
+  const getToken = async() => {
+    const token = await messaging().getToken()
+    console.log("Token = ", token)
+  }
+
+  useEffect(() => {
+    requestUserPermission()
+    getToken()
+  },[])
+
   return (
     <NavigationContainer>
       <MyStack />
-      </NavigationContainer>
+    </NavigationContainer>
   );
 };
 
 export default App;
-
