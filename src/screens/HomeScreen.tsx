e4a3e281d79fac6aae5faf447e3180ac092cd56f
sawy22417 <sawy22417@gmail.com>
2023-10-12 17:33:33 +0300

    finish fire store

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import {Image, Text, View} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
 const HomeScreen = () => {
-  return (
-    <View style={{justifyContent:"center", alignItems:"center", flex:1}}>
-      <Text>HomeScreen</Text>
-    </View>
-  )
+
+  const [dish, setDish] = useState()
+
+const getData = async () =>{
+  const foodsCollection = await firestore().collection('foods').get()
+  console.log(foodsCollection.docs[0].data())
+  setDish(foodsCollection.docs[0].data())
 }
 
-export default HomeScreen
+useEffect(() => {
+  getData()
+}, [])
 
-const styles = StyleSheet.create({})
+  return (
+    <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+      <Text style={{fontSize: 30, color: 'black'}}>Home Screen</Text>
+      <Text>{dish.price}</Text>
+      <Text>{dish.title}</Text>
+      <Image source={{uri:dish.image_url}} style={{height: 100, width:100}}/>
+    </View>
+  );
+};
+
+export default HomeScreen;
