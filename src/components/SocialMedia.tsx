f40489b24f56b48031b2b43fa4e4e0892d4cac72
sawy22417 <sawy22417@gmail.com>
2023-10-12 17:54:18 +0300

    format code

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -1,39 +1,50 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {FC} from 'react';
 
-const SocialMedia = () => {
+const SocialMedia: FC<{onGooglePress: () => void}> = ({onGooglePress}) => {
   return (
     <View style={styles.container}>
-      <Image 
-        source={require("../assets/SocialMediaIcons/facebook.png")}
+      <Image
+        source={require('../assets/SocialMediaIcons/facebook.png')}
         style={styles.image}
       />
-      <Image 
-        source={require("../assets/SocialMediaIcons/twitter.png")}
+      <Image
+        source={require('../assets/SocialMediaIcons/twitter.png')}
         style={styles.twitterIcon}
       />
-      <Image 
-        source={require("../assets/SocialMediaIcons/google.png")}
-        style={styles.image}
-      />
+      <TouchableOpacity onPress={onGooglePress}>
+        <Image
+          source={require('../assets/SocialMediaIcons/google.png')}
+          style={styles.image}
+        />
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default SocialMedia
+export default SocialMedia;
 
 const styles = StyleSheet.create({
-    image:{
-        height: 40,
-        width:40,
-    },
-    twitterIcon:{
-        height:60, width:60, marginTop: 15
-    },
-    container:{
-        flexDirection:"row",
-        justifyContent:"space-evenly",
-        width:"100%",
-        alignItems:"center"
-    }
-})
+  image: {
+    height: 40,
+    width: 40,
+  },
+  twitterIcon: {
+    height: 60,
+    width: 60,
+    marginTop: 15,
+  },
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '100%',
+    alignItems: 'center',
+  },
+});
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -7,29 +7,29 @@ import {
   Text,
   Alert,
 } from 'react-native';
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import MyButton from '../components/MyButton';
 import MyTextInput from '../components/MyTextInput';
 import SocialMedia from '../components/SocialMedia';
-import auth from "@react-native-firebase/auth"
+import auth from '@react-native-firebase/auth';
 
 const LoginScreen = ({navigation}) => {
-
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const loginWithEmailAndPass = () => {
-    auth().signInWithEmailAndPassword(email, password)
-    .then((res) => {
-      console.log(res)
-      Alert.alert("Success: Logged In")
-      navigation.navigate("HomeScreen")
-    })
-    .catch(err =>{ 
-      console.log(err)
-      Alert.alert(err.nativeErrorMessage)
-    })
-  }
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(res => {
+        console.log(res);
+        Alert.alert('Success: Logged In');
+        navigation.navigate('HomeScreen');
+      })
+      .catch(err => {
+        console.log(err);
+        Alert.alert(err.nativeErrorMessage);
+      });
+  };
 
   return (
     <View style={styles.container}>
@@ -44,19 +44,28 @@ const LoginScreen = ({navigation}) => {
         <Text style={styles.title}>Fatmore</Text>
 
         <View style={styles.inputsContainer}>
-            <MyTextInput value={email} onChangeText={text => setEmail(text)} placeholder="Enter E-mail or User Name"/>
-            <MyTextInput value={password} onChangeText={text => setPassword(text)} placeholder="Password" secureTextEntry/>
+          <MyTextInput
+            value={email}
+            onChangeText={text => setEmail(text)}
+            placeholder="Enter E-mail or User Name"
+          />
+          <MyTextInput
+            value={password}
+            onChangeText={text => setPassword(text)}
+            placeholder="Password"
+            secureTextEntry
+          />
 
-            <Text style={styles.textDontHave}>Don't Have An Account Yet? <Text style={{textDecorationLine:"underline"}}>Sign Up</Text></Text>
-            <MyButton title={"Login"} onPress={loginWithEmailAndPass}/>
+          <Text style={styles.textDontHave}>
+            Don't Have An Account Yet?{' '}
+            <Text style={{textDecorationLine: 'underline'}}>Sign Up</Text>
+          </Text>
+          <MyButton title={'Login'} onPress={loginWithEmailAndPass} />
 
-            <Text style={styles.orText}>OR</Text>
+          <Text style={styles.orText}>OR</Text>
 
-            <SocialMedia />
-
-
+          <SocialMedia />
         </View>
-
       </ImageBackground>
     </View>
   );
@@ -80,32 +89,34 @@ const styles = StyleSheet.create({
     position: 'absolute',
     right: 20,
     top: Platform.OS == 'android' ? 20 : 50,
-  },title:{
-    fontSize: 40,
-    color:"white",
-    marginTop: Platform.OS == "android" ? 60 : 110,
-    fontFamily: "Audiowide-Regular"
-  },inputsContainer:{
-    height: 450,
-    width:"100%",
-    backgroundColor:"white",
-    borderRadius: 20,
-    justifyContent:"center",
-    alignItems:"center",
-    marginTop: 30,
-    paddingHorizontal:20
   },
-  textDontHave:{
-    alignSelf:"flex-end",
+  title: {
+    fontSize: 40,
+    color: 'white',
+    marginTop: Platform.OS == 'android' ? 60 : 110,
+    fontFamily: 'Audiowide-Regular',
+  },
+  inputsContainer: {
+    height: 450,
+    width: '100%',
+    backgroundColor: 'white',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 30,
+    paddingHorizontal: 20,
+  },
+  textDontHave: {
+    alignSelf: 'flex-end',
     marginRight: 10,
-    color:"black", 
+    color: 'black',
     marginBottom: 15,
     // fontFamily:"Langar-Regular"
   },
-  orText:{
-        fontSize: 20,
-        color:"gray",
-        marginTop: 20,
-        fontFamily:"Audiowide-Regular"
-  }
+  orText: {
+    fontSize: 20,
+    color: 'gray',
+    marginTop: 20,
+    fontFamily: 'Audiowide-Regular',
+  },
 });
